1cef64013bd8ab25ece9c21d5a6e03314c96b527
AllanSechrist <sechristallan@gmail.com>
2020-12-31 22:57:09 -0800

    Add initial files initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// $(window).resize(function(){
+//   console.log('resize called');
+//   var width = $(window).width();
+//   if(width > 900){
+//     $('h1').removeClass('center');
+//   }
+// })
+// .resize();
